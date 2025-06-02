fded59d783e067bd12fb52f5a5b67261191a9148
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2025-06-02 20:10:57 +0200

    Deleted from main folder

--- detectorLatidos/R/signal_data_input/convert_signal_to_millivolts.R
+++ detectorLatidos/R/signal_data_input/convert_signal_to_millivolts.R
@@ -7,7 +7,6 @@ convert_signal_to_millivolts = function(signal_data, adc_gain, baseline, samplin
   for (s in 1:ncol(signal_data)) {
     signal_name = colnames(signal_data)[s]
     converted_list[[signal_name]] = (signal_data[[s]] - baseline[s]) / adc_gain[s]
-
   }
 
   time = seq(0, length.out = num_samples) / sampling_rate

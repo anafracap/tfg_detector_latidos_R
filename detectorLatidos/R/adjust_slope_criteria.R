48bd96b304c59ca9c955e7df8658491adbcf116a
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2025-06-18 19:43:35 +0200

    Refactoring minimum and maximum slope criteria

--- detectorLatidos/R/adjust_slope_criteria.R
+++ detectorLatidos/R/adjust_slope_criteria.R
@@ -2,15 +2,10 @@ adjust_slope_criteria = function (sample_num_for_slopes, samples_per_2s, num_slo
                                   slope_crit, slope_crit_min, slope_crit_max) {
   if (sample_num_for_slopes %% samples_per_2s == 0){
     if (num_slope == 0){
-      slope_crit = slope_crit - slope_crit %/% 16
-      if (slope_crit < slope_crit_min){
-        slope_crit = slope_crit_min
-      }
+      slope_crit = max (slope_crit_min, slope_crit - slope_crit %/% 16)
+
     } else if (num_slope >=5){
-      slope_crit = slope_crit + slope_crit %/% 16
-      if (slope_crit > slope_crit_max){
-        slope_crit = slope_crit_max
-      }
+      slope_crit = min (slope_crit_max, slope_crit + slope_crit %/% 16)
     }
   }
   return(slope_crit)

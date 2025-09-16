83914a51a6462bfabe38ff7d768ee6b09dd15242
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2025-09-16 20:41:04 +0200

    Tested wrong numbers inputted for variables

--- detectorLatidos/tests/testthat/test-qrs_detection.R
+++ detectorLatidos/tests/testthat/test-qrs_detection.R
@@ -22,4 +22,14 @@ test_that("Wrong format inputted in other variables", {
                              to_sample = "Inf"), "Error: 'to_sample' must be numeric." )
 })
 
-
+test_that("Wrong numbers inputted for variables", {
+  df = data.frame(c(1, 2, 3))
+  expect_error(qrs_detection(df, sampling_rate = -360, threshold = 80, from_sample = 0,
+                             to_sample = Inf), "Error: 'sampling_rate' must be positive" )
+  expect_error(qrs_detection(df, sampling_rate = 360, threshold = -1, from_sample = 0,
+                             to_sample = Inf), "Error: 'threshold' must be non-negative" )
+  expect_error(qrs_detection(df, sampling_rate = 360, threshold = 80, from_sample = -1,
+                             to_sample = Inf), "Error: 'from_sample' must be non-negative" )
+  expect_error(qrs_detection(df, sampling_rate = 360, threshold = 80, from_sample = 10,
+                             to_sample = 5), "Error: 'to_sample' must be greater than 'from_sample'." )
+})
